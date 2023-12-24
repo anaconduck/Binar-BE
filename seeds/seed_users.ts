642293345bdb6f88588143ef00e19b08febc42b9
anaconduck <wahyukusuma29@gmail.com>
2023-12-24 23:04:34 +0700

    fixed seed

--- seeds/seed_users.ts
+++ seeds/seed_users.ts
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 
 export async function seed(knex: Knex): Promise<void> {
   const SALT = bcrypt.genSaltSync(10);
-  const password = bcrypt.hashSync("superadmin", SALT);
+  const password = bcrypt.hashSync("alex1234", SALT);
 
   // Deletes ALL existing entries
   await knex("users").del();
@@ -12,7 +12,7 @@ export async function seed(knex: Knex): Promise<void> {
   await knex("users").insert([
     {
       name: "superadmin",
-      email: "superadmin@superadmin.com",
+      email: "alex@gmail.com",
       password,
       role: "superadmin",
     },
